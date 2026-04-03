9b95ad782766e7594889ce10079f13c9b568b6ea
Md Momin Ali <momin550550@gmail.com>
2026-04-03 15:50:10 +0600

    header and headerclient update file

--- components/frontend/Header.tsx
+++ components/frontend/Header.tsx
@@ -1,21 +1,27 @@
-import { connectDB } from "@/lib/db";
-import Category from "@/models/Category";
+// import { connectDB } from "@/lib/db";
+// import Category from "@/models/Category";
 import HeaderClient from "@/components/frontend/HeaderClient";
 
-export default async function Header() {
-  await connectDB();
+// export default async function Header() {
+//   await connectDB();
 
-  const rawCategories = await Category.find({ isActive: true })
-    .select("_id name slug")
-    .sort({ createdAt: -1 })
-    .limit(6)
-    .lean();
+//   const rawCategories = await Category.find({ isActive: true })
+//     .select("_id name slug")
+//     .sort({ createdAt: -1 })
+//     .limit(6)
+//     .lean();
 
-  const categories = rawCategories.map((cat) => ({
-    _id: String(cat._id),
-    name: cat.name,
-    slug: cat.slug,
-  }));
+//   const categories = rawCategories.map((cat) => ({
+//     _id: String(cat._id),
+//     name: cat.name,
+//     slug: cat.slug,
+//   }));
 
-  return <HeaderClient categories={categories} />;
+//   return <HeaderClient categories={categories} />;
+// }
+
+// import HeaderClient from "@/components/frontend/HeaderClient";
+
+export default function Header() {
+  return <HeaderClient />;
 }
